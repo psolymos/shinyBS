86c427aed776fc6d5c4efcee0112e4c22c81a95e
Peter Solymos <psolymos@gmail.com>
2022-04-09 21:50:43 -0600

    Update for jQuery > 4.1
    https://stackoverflow.com/questions/51960211/error-no-method-named-destroy-for-popover-bootstrap

--- inst/www/shinyBS.js
+++ inst/www/shinyBS.js
@@ -201,10 +201,10 @@ shinyBS.addTooltip = function(id, type, opts) {
   opts = $.extend(opts, dopts);
   
   if(type == "tooltip") {
-    $id.tooltip("destroy");
+    $id.tooltip("dispose");
     $id.tooltip(opts);
   } else if(type == "popover") {
-    $id.popover("destroy");
+    $id.popover("dispose");
     $id.popover(opts);
   }
   
@@ -213,9 +213,9 @@ shinyBS.addTooltip = function(id, type, opts) {
 shinyBS.removeTooltip = function(id, type) {
   var $id = shinyBS.getTooltipTarget(id);
   if(type == "tooltip") {
-    $id.tooltip("destroy");
+    $id.tooltip("dispose");
   } else if(type == "popover") {
-    $id.popover("destroy");
+    $id.popover("dispose");
   }
 }
 
